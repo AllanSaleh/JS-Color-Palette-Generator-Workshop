9c53112ad31b74d1ac7b05c8988fcce5ce5bd85e
Allan <allanahmedd@gmail.com>
2026-02-08 15:48:10 -0500

    implemented the lock feature to lock colors and not have them to be generated

--- scripts.js
+++ scripts.js
@@ -8,8 +8,12 @@ function rgbToHex(r, g, b) {
   return '#' + [r, g, b].map(value => value.toString(16).padStart(2, '0')).join('');
 }
 
+// Generates a random color for each column and adds the hex color code to the p of the column
 function generateColors() {
   for (const color of colors) {
+    // Skip if this color is locked
+    if (color.getAttribute("data-locked") === 'true') continue;
+
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
@@ -23,24 +27,41 @@ function generateColors() {
     const p = color.querySelector('p');
     p.textContent = hexString
   }
-}
 
-generateBtn.addEventListener('click', () => {
-  generateColors();
   generateBtn.classList.add('rotate'); //start rotation
   setTimeout(() => {
     generateBtn.classList.remove('rotate'); //stop rotation
-  }, 500)
-});
+  }, 500);
+}
+
+generateBtn.addEventListener('click', () => generateColors());
 
 window.addEventListener('DOMContentLoaded', generateColors);
 
 window.addEventListener("keydown", (event) => {
   if (event.code === 'Space' || event.key.toLowerCase() === 'g') {
     generateColors();
-    generateBtn.classList.add('rotate'); //start rotation
-    setTimeout(() => {
-      generateBtn.classList.remove('rotate'); //stop rotation
-    }, 500);
   }
 })
+
+// Add click event to each lock icon to lock/unlock color
+for (const color of colors) {
+  const lockSpan = color.querySelector(".lock-icon");
+  // console.log(lockSpan)
+
+  if (lockSpan) {
+    lockSpan.innerHTML = '<i class="fa-solid fa-unlock"></i>'
+    lockSpan.addEventListener('click', () => {
+      const isLocked = color.getAttribute('data-locked') === 'true';
+      
+      if (isLocked === true){
+        color.setAttribute('data-locked', 'false');
+        lockSpan.innerHTML = '<i class="fa-solid fa-unlock"></i>'
+      } else {
+        color.setAttribute('data-locked', 'true');
+        lockSpan.innerHTML = '<i class="fa-solid fa-lock"></i>'
+      }
+
+    })
+  }
+}
